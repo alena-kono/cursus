4919f0f2fa7f7d6c47a1e246d5d1af35ee28e193
Alena Konovalova <konovalova.alena@gmail.com>
2021-01-02 21:02:16 +0300

    Add simple home page

--- cursus_app/__init__.py
+++ cursus_app/__init__.py
@@ -1,10 +1,17 @@
 from flask import Flask
 from cursus_app.db import db
+from cursus_app.home.views import home_blueprint
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(
+        __name__,
+        template_folder='templates',
+        static_folder="static",
+        static_url_path="/"
+        )
     app.config.from_pyfile("config.py")
     db.init_app(app)
+    app.register_blueprint(home_blueprint)
 
     return app
---
+++ cursus_app/home/views.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, render_template
+
+home_blueprint = Blueprint("home", __name__)
+
+
+@home_blueprint.route("/")
+def home_index():
+    page_title = "Cursus - Home page"
+    blocks_to_display = [
+        "Topics",
+        "Courses"
+    ]
+    return render_template(
+        "home/index.html",
+        page_title=page_title,
+        blocks=blocks_to_display
+    )
