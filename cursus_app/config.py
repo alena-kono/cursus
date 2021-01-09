a9e7899529cb115a0ad01435506ecaa360eb9840
Alena Konovalova <konovalova.alena@gmail.com>
2021-01-09 13:03:30 +0300

    Use dotenv for setting up flask_app

--- cursus_app/config.py
+++ cursus_app/config.py
@@ -1,5 +1,7 @@
 import pathlib
 
+import dotenv
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 basedir = pathlib.Path(__file__).parents[1]
@@ -8,4 +10,4 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///' + str(pathlib.Path.joinpath(
     'cursus_app.db'
     ))
 
-SECRET_KEY = pathlib.os.getenv("FLASK_SECRET_KEY")
+SECRET_KEY = dotenv.get_key(".env", "FLASK_SECRET_KEY")
