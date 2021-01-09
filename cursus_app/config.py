3865060d06b48ee2c3ee6bc4d559f5b323f391fe
Alena Konovalova <konovalova.alena@gmail.com>
2021-01-09 14:22:10 +0300

    Add dotenv to configure SQLALCHEMY_DATABASE_URI

--- cursus_app/config.py
+++ cursus_app/config.py
@@ -5,9 +5,11 @@ import dotenv
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 basedir = pathlib.Path(__file__).parents[1]
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + str(pathlib.Path.joinpath(
-    basedir,
-    'cursus_app.db'
+SQLALCHEMY_DATABASE_URI = (
+    dotenv.get_key(".env", "SQLALCHEMY_DATABASE_URI")
+    ) or ("sqlite:///" + str(pathlib.Path.joinpath(
+        basedir,
+        "cursus_app.db")
     ))
 
 SECRET_KEY = dotenv.get_key(".env", "FLASK_SECRET_KEY")
