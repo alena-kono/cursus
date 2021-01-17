dfb2abcf7e0705eb99f84e432330e2dc8987381e
Alena Konovalova <konovalova.alena@gmail.com>
2021-01-17 23:17:44 +0300

    Add docs and simple validation to NewCourseForm

--- cursus_app/course/forms.py
+++ cursus_app/course/forms.py
@@ -1,17 +1,32 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, ValidationError
 
 
-class NewCourse(FlaskForm):
+class NewCourseForm(FlaskForm):
+    """Subclass of :class: `flask_wtf.Flask-Form`.
+    Represents form for adding a new `Course`
+
+    :attr title: StringField
+
+    :attr description: TextAreaField
+
+    :attr submit: SubmitField
+    """
     title = StringField(
         label="Title",
-        validators=[DataRequired(), Length(max=10)],
+        validators=[
+            DataRequired(),
+            Length(max=32, message="Title should be 32 characters max")
+            ],
         render_kw={"class": "form-control", "placeholder": "Title"}
         )
     description = TextAreaField(
         label="Description",
-        validators=[DataRequired()],
+        validators=[
+            DataRequired(),
+            Length(max=255, message="Description should be 255 characters max")
+            ],
         render_kw={
             "class": "form-control",
             "placeholder": "Description", "rows": 5}
@@ -20,3 +35,21 @@ class NewCourse(FlaskForm):
         label="Create",
         render_kw={"class": "btn btn-primary btn-lg mb-4 w-100"}
         )
+
+    def validate_title_chars(self, title: str) -> ValidationError:
+        """Validates `title` by checking whether all characters
+        except the whitespace is alphanumeric (letters(a-z) and/or digits(0-9))
+
+        :param title: field of :class:`NewCourseForm` to be validated
+        :type title: str
+
+        :raises ValidationError: if `title` contains
+        non-alphanumeric characters
+
+        :returns: None
+        :rtype: None
+        """
+        title = title.replace(" ", "")
+        if not title.isalnum():
+            msg = "Title should contain only letters(a-z) and/or digits(0-9)"
+            raise ValidationError(msg)
