36252887e224a33702702340d5809508282731ad
Alena Konovalova <konovalova.alena@gmail.com>
2021-01-20 17:01:03 +0300

    Start to implement lesson indexing

--- cursus_app/lesson/models.py
+++ cursus_app/lesson/models.py
@@ -1,4 +1,5 @@
 from cursus_app.db import db
+from cursus_app.course.models import Course
 
 
 class Lesson(db.Model):
@@ -12,7 +13,10 @@ class Lesson(db.Model):
         db.String(), nullable=False
         )
     index = db.Column(
-        db.Integer, nullable=False
+        db.Integer, nullable=True
+        )
+    next_lesson_index = db.Column(
+        db.Integer, nullable=True
         )
     course = db.Column(
         db.Integer, db.ForeignKey("course.id"), nullable=False
@@ -21,16 +25,36 @@ class Lesson(db.Model):
     def __repr__(self):
         return f"<Lesson {self.title}>"
 
-    def save(self, title: str, content: str, index: int, course: int) -> None:
+    def get_last_index(self) -> int:
+        last_les = Lesson.query.filter(
+            Course.id == self.course
+            ).order_by(Lesson.index.desc()).first()
+        if not last_les:
+            return 0
+        return last_les.index
+
+    def save(
+        self,
+        title: str, content: str, course: int, index: int = 0
+            ) -> None:
         """Adds data and commits to the database table `lesson`.
 
-        :param username: :attr: of :class:`Lesson`
-        representing username
-        :type username: str
-
-        :param password: :attr: of :class:`Lesson`
-        representing user password
+        :param title: :attr: of :class:`Lesson`
+        representing title
         :type title: str
+
+        :param content: :attr: of :class:`Lesson`
+        representing content
+        :type content: str
+
+        :param course: :attr: of :class:`Lesson`
+        representing :class:`Course.id` to which `Lesson()` belongs to
+        :type course: int
+
+        :param index: :attr: of :class:`Lesson`
+        representing index (ie serial number of each `Lesson()`
+        in a course with id=`self.course`)
+        :type index: int, default=0
 
         :raises no exceptions:
 
@@ -39,7 +63,9 @@ class Lesson(db.Model):
         """
         self.title = title
         self.content = content
-        self.index = index
         self.course = course
+        if not index:
+            self.index = self.get_last_index() + 1
+
         db.session.add(self)
         db.session.commit()
